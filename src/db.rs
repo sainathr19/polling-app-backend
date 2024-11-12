e6724ccb8e193efbbffc7f5056d1e3461facd315
Sainath Reddy <gsnr1925@gmail.com>
2024-11-12 20:19:59 +0530

    cleanup

--- src/db.rs
+++ src/db.rs
@@ -3,26 +3,22 @@ pub mod user_repo;
 pub mod vote_repo;
 pub mod reg_state_repo;
 pub mod auth_state_repo;
-use std::{collections::HashMap, env, sync::Arc};
-
+use std::env;
 use auth_state_repo::AuthStateRepository;
 use dotenv::dotenv;
-use futures_util::lock::Mutex;
 use mongodb::Client;
 use poll_repo::PollRepository;
 use reg_state_repo::RegStateRepository;
-use tokio::sync::{broadcast, mpsc};
 use user_repo::UserRepository;
 use vote_repo::VoteRepository;
 
-use crate::models::Poll;
 pub struct MongoDB {
     pub poll_collection: PollRepository,
     pub vote_collection: VoteRepository,
     pub reg_state_collection : RegStateRepository,
     pub auth_state_collection : AuthStateRepository,
     pub user_collection : UserRepository,
-    pub sender: Arc<Mutex<std::collections::HashMap<String, mpsc::Sender<Poll>>>>,}
+}
 
 impl MongoDB {
     pub async fn init() -> Self {
@@ -38,14 +34,12 @@ impl MongoDB {
         let user_collection = UserRepository::init(&database);
         let reg_state_collection = RegStateRepository::init(&database);
         let auth_state_collection = AuthStateRepository::init(&database);
-        let sender = Arc::new(Mutex::new(HashMap::new())) ;  
         MongoDB {
             poll_collection,
             vote_collection,
             reg_state_collection,
             auth_state_collection,
             user_collection,
-            sender
         }
     }
 }
